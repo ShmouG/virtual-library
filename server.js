c680d4a47167e2c52af770905f01795b099fa352
Sam Gross <sammiel.gross@gmail.com>
2019-01-15 19:04:14 -0600

    database running

--- server.js
+++ server.js
@@ -1,26 +1,42 @@
-const express = require("express");
+const express = require('express');
 const mongoose = require('mongoose');
-const path = require("path");
-const PORT = process.env.PORT || 3001;
-const app = express();
+const path = require('path');
+const routes = require('./routes');
 const db = mongoose.connection;
 
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 }
 
-// Define API routes here
-
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+app.use('/', routes, (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
+
+const databaseUri = 'mongodb://localhost/vlibrary';
+
+// const MONGODB_URI = process.env.MONGODB_URI;
+// mongoose.connect(MONGODB_URI || { useNewUrlParser: true });
+
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI)
+} else {
+  mongoose.connect(databaseUri)
+};
+
+db.once('error', (err) => {
+  console.log('Mongoose error:', err);
+});
+
+db.once('open', () => {
+  console.log('Mongoose connection successful.')
+})
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
