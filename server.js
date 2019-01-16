0ce18e0ee6d398dadb70bcf22c5a1337a1879b02
Sam Gross <sammiel.gross@gmail.com>
2019-01-16 00:55:50 -0600

    mongoose set

--- server.js
+++ server.js
@@ -14,12 +14,13 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-} else {
+} 
 
-app.use('/', routes, (req, res) => {
-  res.sendFile(path.join('./index.html'));
-});
-};
+app.use(routes);
+
+// Connect to the Mongo DB
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }) || ("mongodb://localhost:27017/vlibrary", { useNewUrlParser: true });
+
 const databaseUri = 'mongodb://localhost:27017/vlibrary';
 
 // mongoose.connect(MONGODB_URI || { useNewUrlParser: true });
